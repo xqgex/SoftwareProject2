129b9f454ee87adf2a1d220c85c05b87d56ba8e5
Alon15 <alon15@gmail.com>
2016-04-25 15:38:56 +0300

    Revert "testing"
    This reverts commit 2b0b88a2466a8c3135f93365cf8f364633de8009.

--- main.cpp
+++ main.cpp
@@ -3,23 +3,24 @@
 #define PI 3.14
 #define EXIT_MSG "Exiting...\n"
 
+
 printf("Enter images directory path:\n");
 fflush(NULL);
 printf("Enter images prefix:\n");
 fflush(NULL);
 printf("Enter number of images:\n");
 fflush(NULL);
-printf("An error occurred - invalid number of images\n");
+	printf("An error occurred - invalid number of images\n");
 fflush(NULL);
 printf("Enter images suffix:\n");
 fflush(NULL);
 printf("Enter number of bins:\n");
 fflush(NULL);
-printf("An error occurred - invalid number of bins\n");
+	printf("An error occurred - invalid number of bins\n");
 fflush(NULL);
 printf("Enter number of features:\n");
 fflush(NULL);
-printf("An error occurred - invalid number of features\n");
+	printf("An error occurred - invalid number of features\n");
 fflush(NULL);
 printf("Enter a query image or # to terminate:\n");
 fflush(NULL);
@@ -34,7 +35,7 @@ printf("Nearest images using local descriptors:\n");
 fflush(NULL);
 printf("j1, j2, j3, j4, j5\n");
 fflush(NULL);
-printf("An error occurred - allocation failure\n");
+	printf("An error occurred - allocation failure\n");
 fflush(NULL);
 
 
